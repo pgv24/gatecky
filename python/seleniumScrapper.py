aff05c4673d470485b13d0c6b70cf889d5fc3769
pgv24 <pgv2495@gmail.com>
2024-10-30 07:52:04 -0600

    PA test

--- python/seleniumScrapper.py
+++ python/seleniumScrapper.py
@@ -19,11 +19,10 @@ search_box.send_keys('leche')
 #Submit the text
 search_box.send_keys(Keys.RETURN)
 
+#corregir esto
 order_items_by_name_button = driver.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div/div[2]/div/div/section/div[2]/div/div/div/div[2]/section/div/div[1]/div/div/div/div/button')
 order_items_by_name_button.click()
 
-select_Nombre_creciente =  driver.find_element(By.CLASS_NAME, "hover-bg-muted-5 bg-base vtex-search-result-3-x-orderByOptionItem  c-on-base f5 ml-auto db no-underline pointer tl bn pv4 ph5 w-100 right-0-ns")
-select_Nombre_creciente.click()
 
 
 
